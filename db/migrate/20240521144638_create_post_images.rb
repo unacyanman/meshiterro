43a81e8c763250a22ce3f353cb2e7af8b1f980f6
unacyanman <166233150+unacyanman@users.noreply.github.com>
2024-05-21 15:09:20 +0000

    [Finish] 4-8

--- db/migrate/20240521144638_create_post_images.rb
+++ db/migrate/20240521144638_create_post_images.rb
@@ -2,11 +2,10 @@ class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
     create_table :post_images do |t|
 
-      t.timestamps
-      t.integer :id #投稿画像を識別する ID
       t.string :shop_name	 #お店の名前
       t.text :caption #画像の説明
       t.integer :user_id #投稿したユーザを識別する ID（users テーブルの id を保存する）
+      t.timestamps
     end
   end
 end
