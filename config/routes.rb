d617cb7f6636d28c6674e6bf69411028051207e8
unacyanman <166233150+unacyanman@users.noreply.github.com>
2024-05-22 13:32:16 +0000

    [Finish] 4-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  get '/about', to: 'homes#about', as: 'about'
+  
+  resources :post_images, only: [:new, :index, :show]
+  
+  get "/homes/about" => "homes#about", as: "about"
+  
 end
